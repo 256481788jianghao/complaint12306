dc8775d0c8681dd91e74e5533e79438ad40515a2
jianghao <256481788@163.com>
2016-08-03 08:29:46 +0800

    增加email地址的输入

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 
+import emailTool
+import sys
+
 complaint_email_addr = {
     "ky":'kyfw@12306.cn',
     "hy":'hyfw@12306.cn',
     "xb":'xbfw@13306.cn'
 }
 
-print(complaint_email_addr)
+cfg = {
+    'Host':'smtp.163.com',
+    'From':sys.argv[1],
+    'PW':sys.argv[2],
+    'To':sys.argv[3],
+    'subject':'my dream',
+    'content':'I think this is my problem.\n'
+}
+tool = emailTool.emailTool(cfg)
+#tool.sendDelay(10)
+tool.sendcyc(5*4*60,5,cfg['content'])
